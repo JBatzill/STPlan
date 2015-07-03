2df45b1b39cfa06a2eb597088c05081e91ca3c1d
PureAwesomeness <johannesbatzill@live.de>
2015-07-03 15:54:05 -0400

    add useless import in main.py in rest_server ... workaround?

--- server/rest_server/main.py
+++ server/rest_server/main.py
@@ -4,6 +4,7 @@ from flask import request
 from datetime import timedelta
 from rest_server.definitions import *
 from rest_server.database import query_db
+from database import submit_db
 from database.sql_commands import SQL_READ_STD
 from logger import get_log_rest
 import json
