3cc325d0d36308955541cb82b0f40aac65ca6e33
PureAwesomeness <johannesbatzill@live.de>
2015-07-07 21:34:26 -0400

    updated database design

--- server/database/sql_commands/stplan_schema.sql
+++ server/database/sql_commands/stplan_schema.sql
@@ -1,9 +1,23 @@
+DROP TABLE IF EXISTS school;
+CREATE TABLE school (
+  _id INTEGER NOT NULL AUTO_INCREMENT,
+  _name VARCHAR(128),
+  _shortcut VARCHAR(16),
+  _city VARCHAR(32),
+  _state VARCHAR(32),
+  _country VARCHAR(32),
+  _schedule_url TEXT,
+  _last_update_info TEXT
+);
+
+
 DROP TABLE IF EXISTS schedule;
 CREATE TABLE schedule (
-  _school VARCHAR(64),
-  _date DATE,
-  _class VARCHAR(16),
-  _time TINYINT,
+  _id INTEGER NOT NULL AUTO_INCREMENT,
+  _school_id INTEGER NOT NULL,
+  _date DATE NOT NULL,
+  _class VARCHAR(16) NOT NULL,
+  _time VARCHAR(32),
   _subject VARCHAR(32),
   _teacher VARCHAR(32),
   _new_subject VARCHAR(32),
@@ -11,10 +25,26 @@ CREATE TABLE schedule (
   _new_room VARCHAR(32),
   _origin VARCHAR(64),
   _treatment VARCHAR(64),
-  _reason VARCHAR(64),
+  _note TEXT,
 
-  PRIMARY KEY(_school, _date, _class, _time, _subject)
+  PRIMARY KEY(_id),
+  FOREIGN KEY(_school_id) REFERENCES school(_id)
 );
 
-CREATE INDEX main_index
-ON schedule(_school, _date, _class);
+CREATE INDEX index_schedule_main
+ON schedule(_school_id, _date, _class);
+
+DROP TABLE IF EXISTS notification;
+CREATE TABLE notification (
+  _id INTEGER NOT NULL AUTO_INCREMENT,
+  _school_id INTEGER,
+  _date DATE,
+  _title TEXT,
+  _message TEXT,
+
+  PRIMARY KEY(_id),
+  FOREIGN KEY(_school_id) REFERENCES school(_id)
+);
+
+CREATE INDEX index_notification_main
+ON notification(_school_id, _date);
